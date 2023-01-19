b58000f1057f268be4f0c3e9156d3412433be600
Tenzin wangyal <tenzinwangyal20@yahoo.com>
2023-01-19 15:13:37 -0500

    Initial Commit

---
+++ CircularTimer/CircularTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularTimerApp.swift
+//  CircularTimer
+//
+//  Created by Tenzin wangyal on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct CircularTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
